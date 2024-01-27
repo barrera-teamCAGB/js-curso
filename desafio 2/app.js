0cc709be89549ba1a2de74f49c63dbfeb7e5e730
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-27 15:20:52 -0600

    Desafio 1: Crear una función que muestre "¡Hola, mundo!" en la consola.

---
+++ desafio 2/app.js
@@ -0,0 +1,10 @@
+// Resolución del desafío 2
+// Saber cómo usar funciones es uno de los conceptos fundamentales en la programación y el desarrollo de software. Las funciones desempeñan un papel crucial en la organización, modularización y reutilización del código.
+// crearemos un html basico con botones para generar cada desafio
+
+// Desafio 1: Crear una función que muestre "¡Hola, mundo!" en la consola.
+function bienvenida() {
+    console.log("¡Hola, mundo!");
+    return;
+}
+
