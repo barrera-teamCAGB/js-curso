d3d0a57f82521ded247b3962313176029238a7e7
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-02-01 16:50:06 -0600

    Desafio 1: Crea una lista vacía llamada "listaGenerica".

---
+++ desafio-4/app.js
@@ -0,0 +1,7 @@
+// Resolución del desafío 4
+// Saber cómo usar las funciones es uno de los conceptos fundamentales en la programación y el desarrollo de software. Las funciones desempeñan un papel crucial en la organización, modularización y reutilización del código.
+
+// al termino deberiamos poder modificar y poder interactual con el html, para eso es necesario crear uno, mientras sera solo codigo en js
+
+// Desafio 1: Crea una lista vacía llamada "listaGenerica".
+let listaGenerica = [];
